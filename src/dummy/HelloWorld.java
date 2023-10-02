e05f8c74396ad5d3a8da92458a6a74a4bce716fd
Drake <dferrel@emich.edu>
2023-10-02 18:44:16 -0400

    thank you come again

--- src/dummy/HelloWorld.java
+++ src/dummy/HelloWorld.java
@@ -3,5 +3,6 @@ package dummy;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Thunder Bolts And Lightning Very Very Frightening Me");
 	}
 }
