860b48644cd3d1b5355d701dd1f55d4c64862e69
Drake <dferrel@emich.edu>
2023-10-02 19:03:14 -0400

    do it

--- src/dummy/HelloWorld.java
+++ src/dummy/HelloWorld.java
@@ -3,6 +3,6 @@ package dummy;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("alternate And conflict Very Very Frightening Me");
+		System.out.println("scaramouche! scaramouche!");
 	}
 }
